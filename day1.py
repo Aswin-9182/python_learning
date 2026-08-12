0b2440f2cbed79cb2c36a1d570c374c6ee6f4f20
Aswin <aswingaddigopula@gmail.com>
2026-08-12 18:13:48 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,56 @@
+# Tokens & Statements
+name="Aswin"
+Age=20
+City= "Hyderabad"
+print(name)
+print(Age)
+print(City)
+
+# Identifiers
+marks=90
+print(name)
+print(Age)
+print(marks)
+
+#single line comment
+print(name)
+print(Age)
+
+# Multiple line comments
+'''
+Welcome to python
+I am learning python
+Python is easy to learn'''
+# variables
+height=6.0
+student=True
+print(height)
+print(student)
+
+#multiple assignmeent
+name,age,city="aswin",20,"Hyderabad"
+print(name)
+print(age)
+print(city)
+
+# Reassignment
+age=20
+print(age)
+age=25
+print(age)
+
+# swaping of two numbers
+a=10
+b=20
+a,b=b,a
+print(a)
+print(b)
+
+# Deleting a variables
+print(a)
+del a
+
+#keywords
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
